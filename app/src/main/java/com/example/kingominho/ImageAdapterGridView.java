9a3ed29415db70fd124b7c61a6050f0a821a4bac
the-it-weirdo <ddsmegh4@gmail.com>
2019-09-15 00:50:47 +0530

    changes app icon. implements shimmer effect: com.facebook.shimmer.ShimmerFrameLayout (check gradle dependencies)

--- app/src/main/java/com/example/kingominho/ImageAdapterGridView.java
+++ app/src/main/java/com/example/kingominho/ImageAdapterGridView.java
@@ -6,7 +6,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+
 
 public class ImageAdapterGridView extends BaseAdapter {
     private Context context;
--- app/src/main/java/com/example/kingominho/WebViewFragment.java
+++ app/src/main/java/com/example/kingominho/WebViewFragment.java
@@ -20,8 +20,9 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 
 /**
@@ -33,12 +34,9 @@ import android.widget.Toast;
  * create an instance of this fragment.
  */
 public class WebViewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "url";
     //private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     //private String mParam2;
 
@@ -55,16 +53,15 @@ public class WebViewFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * //@param param2 Parameter 2.
      * @return A new instance of fragment WebViewFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static WebViewFragment newInstance(String param1, String param2) {
+    public static WebViewFragment newInstance(String param1){ //String param2) {
         WebViewFragment fragment = new WebViewFragment();
-        //Bundle args = new Bundle();
-        //args.putString(ARG_PARAM1, param1);
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
         //args.putString(ARG_PARAM2, param2);
-        //fragment.setArguments(args);
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -91,10 +88,21 @@ public class WebViewFragment extends Fragment {
         getActivity().setTitle(getResources().getString(R.string.webViewLabel));
 
         webView = (WebView) getView().findViewById(R.id.webView);
-        //ProgressBar progressBar = new ProgressBar();
+
+        webView.setVisibility(View.GONE);
+
+        final ShimmerFrameLayout container = (ShimmerFrameLayout) getView().findViewById(R.id.shimmer_view_container);
 
 
         webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                container.stopShimmer();
+                container.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
+            }
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
